a99b0452aa599c41ff366eaa068755ce9f3adf82
NguyenTien <nguyenvantien0620@gmail.com>
2023-08-23 02:29:29 +0700

    refactor

--- src/helpers/createPhotoStrip.js
+++ src/helpers/createPhotoStrip.js
@@ -110,3 +110,56 @@ export function applyFilterToImage(base64Image, width, height, filterName) {
     image.src = base64Image;
   });
 }
+
+export function drawImagesOnCanvasTest1240(imageUrls, canvasWidth, canvasHeight, backgroundUrl) {
+  const elementWidth = canvasWidth / 2;
+  const elementHeight = canvasHeight;
+  return new Promise((resolve, reject) => {
+      const c = document.createElement("canvas");
+      c.width = canvasWidth;
+      c.height = canvasHeight;
+      const ctx = c.getContext("2d");
+    
+      // Load the background image
+      const background = new Image();
+      background.onload = () => {
+        ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
+  
+        // Load the images
+        const imageObjs = [];
+        let imagesLoaded = 0;
+        const imageHeight = 380;
+        const imageWidth = 560;
+        const margin = (elementWidth - imageWidth) / 2; // Margin between images
+        for (let i = 0; i < imageUrls.length; i++) {
+          const imageObj = new Image();
+          imageObj.onload = onImageLoad;
+          imageObj.src = imageUrls[i];
+          imageObjs.push(imageObj);
+        }
+  
+        // Callback function that is called when an image has finished loading
+        function onImageLoad() {
+          imagesLoaded++;
+  
+          if (imagesLoaded === imageObjs.length) {
+            // All images have finished loading, draw them onto the canvas
+            let x = margin; // Start at the left with some margin
+            let y = margin; // Start at the top with some margin
+            for (let i = 0; i < imageObjs.length; i++) {
+              const imageObj = imageObjs[i];
+              ctx.drawImage(imageObj, x, y , imageWidth, imageHeight);
+              ctx.drawImage(imageObj, x + elementWidth - 15, y , imageWidth, imageHeight);
+              y += imageHeight + margin; // Update the y position for the next image
+            }
+  
+            // Get the data URL of the canvas
+            // Resolve the promise with the data URL and the resulting image
+            resolve(c.toDataURL('image/png'));
+          }
+        }
+      };
+      background.onerror = error => reject(error);
+      background.src = backgroundUrl
+    });
+}
